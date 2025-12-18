7421eb4976edeeab81185c7631f93228880f35fb
bala-amypo <bala@amypo.in>
2025-12-18 16:42:54 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,41 +1,41 @@
-// package com.example.demo.service.impl;
+package com.example.demo.service.impl;
 
-// import com.example.demo.entity.User;
-// import com.example.demo.exception.ValidationException;
-// import com.example.demo.repository.UserRepository;
-// import com.example.demo.service.UserService;
-// import org.springframework.stereotype.Service;
+import com.example.demo.entity.User;
+import com.example.demo.exception.ValidationException;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
+import org.springframework.stereotype.Service;
 
-// import java.util.List;
+import java.util.List;
 
-// @Service
-// public class UserServiceImpl implements UserService {
+@Service
+public class UserServiceImpl implements UserService {
 
-//     private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
-//     // constructor injection
-//     public UserServiceImpl(UserRepository userRepository) {
-//         this.userRepository = userRepository;
-//     }
+    // constructor injection
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-//     @Override
-//     public User registerUser(User user) {
-//         if (userRepository.existsByEmail(user.getEmail())) {
-//             throw new ValidationException("email exists");
-//         }
-//         if (user.getRole() == null) {
-//             user.setRole("USER");
-//         }
-//         return userRepository.save(user);
-//     }
+    @Override
+    public User registerUser(User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new ValidationException("email exists");
+        }
+        if (user.getRole() == null) {
+            user.setRole("USER");
+        }
+        return userRepository.save(user);
+    }
 
-//     @Override
-//     public User getUser(Long id) {
-//         return userRepository.findById(id).orElse(null);
-//     }
+    @Override
+    public User getUser(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
 
-//     @Override
-//     public List<User> getAllUsers() {
-//         return userRepository.findAll();
-//     }
-// }
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}

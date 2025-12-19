61d06625c23f265137821e563cc01c6b24390e0d
bala-amypo <bala@amypo.in>
2025-12-19 15:52:43 +0000

    User commit

--- src/main/java/com/example/demo/service/ ResourceAllocationService.java
+++ src/main/java/com/example/demo/service/ ResourceAllocationService.java
@@ -1,1 +1,13 @@
 package com.example.demo.service;
+
+import com.example.demo.entity.ResourceAllocation;
+import java.util.List;
+
+public interface ResourceAllocationService {
+
+    ResourceAllocation autoAllocate(Long requestId);
+
+    ResourceAllocation getAllocation(Long id);
+
+    List<ResourceAllocation> getAllAllocations();
+}

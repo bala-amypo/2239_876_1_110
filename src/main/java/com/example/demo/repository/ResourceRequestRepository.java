af786a5d32ee61c50bc0269386068494dbf2f526
bala-amypo <bala@amypo.in>
2025-12-19 06:29:17 +0000

    User commit

--- src/main/java/com/example/demo/repository/ResourceRequestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-public interface ResourceRequestRepository extends JpaRepository<ResourceRequest, Long> {
-    List<ResourceRequest> findByRequestedById(Long userId);
-}
-

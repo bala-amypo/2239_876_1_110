d1924277e25aee00beca456760152a06d1f18ed6
bala-amypo <bala@amypo.in>
2025-12-19 04:38:27 +0000

    User commit

--- src/main/java/com/example/demo/controller/ResourceController.java
+++ src/main/java/com/example/demo/controller/ResourceController.java
@@ -1,33 +1,33 @@
-// package com.example.demo.controller;
+package com.example.demo.controller;
 
-// import com.example.demo.entity.Resource;
-// import com.example.demo.service.ResourceService;
-// import org.springframework.web.bind.annotation.*;
+import com.example.demo.entity.Resource;
+import com.example.demo.service.ResourceService;
+import org.springframework.web.bind.annotation.*;
 
-// import java.util.List;
+import java.util.List;
 
-// @RestController
-// @RequestMapping("/api/resources")
-// public class ResourceController {
+@RestController
+@RequestMapping("/api/resources")
+public class ResourceController {
 
-//     private final ResourceService resourceService;
+    private final ResourceService resourceService;
 
-//     public ResourceController(ResourceService resourceService) {
-//         this.resourceService = resourceService;
-//     }
+    public ResourceController(ResourceService resourceService) {
+        this.resourceService = resourceService;
+    }
 
-//     @PostMapping
-//     public Resource createResource(@RequestBody Resource resource) {
-//         return resourceService.createResource(resource);
-//     }
+    @PostMapping
+    public Resource createResource(@RequestBody Resource resource) {
+        return resourceService.createResource(resource);
+    }
 
-//     @GetMapping
-//     public List<Resource> getAllResources() {
-//         return resourceService.getAllResources();
-//     }
+    @GetMapping
+    public List<Resource> getAllResources() {
+        return resourceService.getAllResources();
+    }
 
-//     @GetMapping("/{id}")
-//     public Resource getResource(@PathVariable Long id) {
-//         return resourceService.getResource(id);
-//     }
-// }
+    @GetMapping("/{id}")
+    public Resource getResource(@PathVariable Long id) {
+        return resourceService.getResource(id);
+    }
+}

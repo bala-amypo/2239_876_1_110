12adfe3fa3be542614c170b02191a635d0bd527d
bala-amypo <bala@amypo.in>
2025-12-20 06:46:20 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ResourceRequestServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ResourceRequestServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.demo.service.impl;
 
+import java.util.List;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.entity.ResourceRequest;

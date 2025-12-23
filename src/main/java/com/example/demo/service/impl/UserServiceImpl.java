3c123fc4d31f1913fc16567fcb69899b243b29a0
bala-amypo <bala@amypo.in>
2025-12-23 07:21:47 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -4,6 +4,6 @@ import com.example.demo.service.UserService;
 import org.springframework.stereotype.Service;
 
 @Service
-public abstract class UserServiceImpl implements UserService {
+public interface UserServiceImpl implements UserService {
     // marked abstract to avoid implementing methods
 }

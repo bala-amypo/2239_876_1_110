a3a693d66e8dec941977bf2b26bcc2d1f5eb789e
bala-amypo <bala@amypo.in>
2025-12-24 06:32:15 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Resource.java
@@ -0,0 +1,24 @@
+package com.example.demo.entity;
+
+public class Resource {
+    private Long id;
+    private String resourceName;
+    private String resourceType;
+    private Integer capacity;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getResourceName() { return resourceName; }
+    public void setResourceName(String resourceName) { this.resourceName = resourceName; }
+
+    public String getResourceType() { return resourceType; }
+    public void setResourceType(String resourceType) { this.resourceType = resourceType; }
+
+    public Integer getCapacity() { return capacity; }
+    public void setCapacity(Integer capacity) {
+        if (capacity == null || capacity < 1)
+            throw new RuntimeException("Capacity must be >=1");
+        this.capacity = capacity;
+    }
+}

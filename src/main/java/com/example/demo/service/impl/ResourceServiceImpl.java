04bd72f4ae6dd99cb6b11dc3724f3730d5f0b59d
bala-amypo <bala@amypo.in>
2025-12-24 13:06:43 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ResourceServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ResourceServiceImpl.java
@@ -3,8 +3,10 @@ package com.example.demo.service.impl;
 import com.example.demo.entity.Resource;
 import com.example.demo.repository.ResourceRepository;
 import com.example.demo.service.ResourceService;
+import org.springframework.stereotype.Service;
 import java.util.List;
 
+@Service
 public class ResourceServiceImpl implements ResourceService {
     private final ResourceRepository resourceRepository;
 

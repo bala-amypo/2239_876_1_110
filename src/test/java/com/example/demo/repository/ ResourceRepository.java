a8bd0eb5bae9dbbc00be9490f2f1709cfaf67bd3
bala-amypo <bala@amypo.in>
2025-12-24 06:41:15 +0000

    User commit

--- src/test/java/com/example/demo/repository/ ResourceRepository.java
+++
@@ -1,6 +0,0 @@
-public interface ResourceRepository {
-    boolean existsByResourceName(String name);
-    List<Resource> findAll();
-    List<Resource> findByResourceType(String type);
-    Resource save(Resource r);
-}

e5cc82e999db6d8df08b7792c30a71344d388ae6
bala-amypo <bala@amypo.in>
2025-12-25 13:23:37 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,6 +1,5 @@
 package com.example.demo.config;
 
-import com.example.demo.security.JwtUtil;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
@@ -9,17 +8,12 @@ import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
-    
-    @Bean
-    public JwtUtil jwtUtil() {
-        return new JwtUtil("test-secret-key-that-is-long-enough-1234", 3600000);
-    }
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .servers(List.of(
-                        new Server().url("https://9129.32procr.amypo.ai")
+                        new Server().url("https://9095.pro604cr.amypo.ai")
                 ));
     }
 }

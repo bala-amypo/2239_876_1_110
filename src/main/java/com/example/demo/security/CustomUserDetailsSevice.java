32245e56c14265f0aa8a641a83648bb2ee5de5ef
bala-amypo <bala@amypo.in>
2025-12-26 09:28:33 +0000

    User commit

--- src/main/java/com/example/demo/security/CustomUserDetailsSevice.java
+++ src/main/java/com/example/demo/security/CustomUserDetailsSevice.java
@@ -1,35 +1,21 @@
 package com.example.demo.security;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.*;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import com.example.demo.entity.User;
-import com.example.demo.repository.UserRepository;
+import java.util.ArrayList;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
-
     @Override
-    public UserDetails loadUserByUsername(String email)
-            throws UsernameNotFoundException {
-
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() ->
-                        new UsernameNotFoundException("User not found"));
-
-        return new org.springframework.security.core.userdetails.User(
-                user.getEmail(),
-                user.getPassword(),
-                List.of(
-                        new SimpleGrantedAuthority("ROLE_" + user.getRole())
-                )
-        );
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return User.builder()
+                .username(username)
+                .password("$2a$10$encoded")
+                .authorities(new ArrayList<>())
+                .build();
     }
 }

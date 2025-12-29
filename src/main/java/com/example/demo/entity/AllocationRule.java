f62cbc16c84541bde9c2862aedd448c830d6c3ca
bala-amypo <bala@amypo.in>
2025-12-29 04:00:57 +0000

    User commit

--- src/main/java/com/example/demo/entity/AllocationRule.java
+++ src/main/java/com/example/demo/entity/AllocationRule.java
@@ -31,7 +31,6 @@ public class AllocationRule {
         this.priorityWeight = priorityWeight != null ? priorityWeight : 0;
     }
 
-    // Getters and Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
